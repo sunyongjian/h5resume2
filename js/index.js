72af3673823a7de39b96d0b53a499c3c2ad28630
sunyongjian <704928775@qq.com>
2016-06-12 01:45:03 +0800

    test3

--- js/index.js
+++ js/index.js
@@ -3,39 +3,35 @@
  */
 
 (function (desW) {
-    var winW=document.documentElement.clientWidth;
-    document.documentElement.style.fontSize=winW/desW*100+"px";
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / desW * 100 + "px";
 
 })(640);
 
-
-var step=0;
-    var mySwiper=new Swiper('.swiper-container',{
-        direction :"vertical",
-        loop:true,
-        onSlidePrevEnd:function () {
-            step--;
-            change();
-            if(step==0){
-                step=6;
-            }
-
-        },
-        onSlideNextEnd:function () {
-            step++;
-            change();
-            if(step==7){
-                step=1;
-            }
+var step = 0;
+var mySwiper = new Swiper('.swiper-container', {
+    direction: "vertical",
+    loop: true,
+    onSlidePrevEnd: function () {
+        step--;
+        change();
+        if (step == 0) {
+            step = 6;
         }
-    });
+    },
+    onSlideNextEnd: function () {
+        step++;
+        change();
+        if (step == 7) {
+            step = 1;
+        }
+    }
+});
 
 function change() {
-    divs=document.querySelectorAll(".swiper-slide");
-
-    [].forEach.call(divs,function (curDiv,index) {
-        console.log(index);
-        curDiv.id=index==step?curDiv.getAttribute("trueId"):null;
+    divs = document.querySelectorAll(".swiper-slide");
+    [].forEach.call(divs, function (curDiv, index) {
+        curDiv.id = index == step ? curDiv.getAttribute("trueId") : null;
     })
 }
 
