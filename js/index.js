b7ef38adc7bf139d1cc5d50bac1702ff5fc4192d
sunyongjian <704928775@qq.com>
2016-06-13 16:42:32 +0800

    test6

--- js/index.js
+++ js/index.js
@@ -7,10 +7,20 @@
     document.documentElement.style.fontSize = winW / desW * 100 + "px";
 
 })(640);
+function fixPagesHeight() {
+    $('.swiper-slide,.swiper-container').css({
+        height: $(window).height(),
+        width:$(window).width()
+    })
+}
+$(window).on('resize', function () {
+    fixPagesHeight();
+});
+fixPagesHeight();
+
 
 var step = 0;
 var arrow=document.querySelector(".arrow");
-console.log(arrow)
 var mySwiper = new Swiper('.swiper-container', {
     direction: "vertical",
     loop: true,
@@ -48,25 +58,19 @@ function change() {
 ~function () {
     var audioBox = document.querySelector(".audio"),
         myAudio = audioBox.getElementsByTagName("audio")[0];
-
-    //->延时播放音频文件,先让其他的内容加载
     window.setTimeout(function () {
         myAudio.play();
         myAudio.addEventListener("canplay", function () {
-            //->当音频可以播放的时候触发这个事件
             audioBox.style.display = "block";
             audioBox.className += " audioMove";
         }, false);
     }, 1000);
-
-    //->点击音乐图标,实现音频的暂停或者播放
     audioBox.addEventListener("click", function () {
-        if (myAudio.paused) {//->当前是暂停的,我让其播放
+        if (myAudio.paused) {
             myAudio.play();
             audioBox.className = "audio audioMove";
             return;
         }
-        //->当前是播放的,我让其暂停
         myAudio.pause();
         audioBox.className = "audio";
     }, false);
